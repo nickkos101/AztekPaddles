319d6a056d68149c183d515cc2f9e23b248d463d
Nickkos <Nickkoskowski@gmail.com>
2014-12-28 20:34:59 -0800

    Added lots of changes to the combinator

--- function.js
+++ function.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 
   $('.slider').slick({});
   $('.blog-slider .post-wrap').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
 
   function moveSlider() {
     $('.slider-load-bar .progress').animate({'width' : '+=100%'}, 3000, function(){
@@ -28,10 +28,79 @@ $(document).ready(function(){
 
   //Prototype JS
 
-  $('.question input, .question textarea, .question select').click(function(){
-    $(this).parent('.question').hide();
-    $(this).parent('.question').next('.question').css('display', 'inline-block');
-    $('.combinator-load-bar .progress').animate({'width':'+=10%'});
-  });
+  function progressBar() {
+    var totalQuestion = $('.question').length;
+    var QuestionPercentage = 1 / (totalQuestion - 1) * 100;
+    $('.combinator-load-bar .progress').animate({'width':'+='+QuestionPercentage+'%'});
+  }
+
+  window.startpage = 1;
+
+  function goToNextQuestion(eve) {
+    window.startpage++;
+    console.log(startpage);
+    $(eve).parent().parent().parent('.question').hide();
+    $(eve).parent().parent().parent('.question').next('.question').css('display', 'inline-block');
+    $('.startQuestion').text(window.startpage);
+  }
+
+  function formProcessor() {
+    $('.question button').click(function(){
+      event.preventDefault();
+      goToNextQuestion(this);
+      progressBar(this);
+    });
+
+    $('.question select').change(function(){
+      event.preventDefault();
+      if (!$(this).hasClass('existing')) {
+        goToNextQuestion(this);
+        progressBar(this);
+      }
+    });
+
+    $('.question input[type=number]').change(function(){
+      event.preventDefault();
+      if(isNaN($(this).val())) {
+        alert('You must enter your weight as a number!');
+      }
+      else {
+        goToNextQuestion(this);
+        progressBar(this);
+      }
+    });
+
+    $('.existing').change(function(){
+      event.preventDefault();
+      if ($('.existing').val() === 'yes') {
+        $(this).fadeOut();
+        $('.subquestion:first-of-type').fadeIn();
+        $('.subquest').text('What type of paddle?');
+      }
+      else {
+        goToNextQuestion(this);
+        progressBar(this);
+      }
+    });
+
+    //Subquestions
+
+    $('.subquestion select').change(function(){
+      $(this).parent().hide();
+      $(this).parent().next().show();
+    });
+
+    $('.subquestion input[type=text]').change(function(){
+      var qtext = $(this).parent().next().find('label').text();
+      $(this).parent().hide();
+      $(this).parent().next().show();
+      $('.subquest').text(qtext);
+    });
+
+    $('.bladetype').text($('.paddleuse').val());
+    $('.shaftlength').text($('.tall').val());
+  }
+
+  formProcessor();
 
 });
